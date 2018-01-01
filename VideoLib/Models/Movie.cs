e4d66f68675014571026b013cfc756461d3c55ca
AbhishekTomar <hunabhi@gmail.com>
2018-01-01 16:07:11 +0530

    mvc with theme

---
+++ VideoLib/Models/Movie.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VideoLib.Models
+{
+    public class Movie
+    {
+        public Int16 Id { get; set; }
+        public String Name { get; set; }
+    }
+}//POCO Plain Old CLR Object 
+//   assuming the action that we want to do choosing a movie falls under 
+//    route  /movie/random movies controller with action called random
